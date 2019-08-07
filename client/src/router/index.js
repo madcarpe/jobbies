eb90efd55447618ea708584cdb1540deb6aec5e6
madcarpe <madcarpe@gmail.com>
2019-08-07 13:44:17 -0500

    Set up the environment for the jobbies board

---
+++ client/src/router/index.js
@@ -0,0 +1,22 @@
+
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Hello from '@/components/Hello'
+import Posts from '@/components/Posts'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // },
+    {
+      path: '/',
+      name: 'Posts',
+      component: Posts
+    }
+  ]
+})
